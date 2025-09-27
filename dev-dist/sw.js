47c2e7028c578bc8079d96bd3dfb489f1213d134
IDGS-801-22001372 <81512@alumnos.utleon.edu.mx>
2025-09-27 01:32:57 -0600

    Correccion

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.93st8v7je1o"
+    "revision": "0.6kp129fmfko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
